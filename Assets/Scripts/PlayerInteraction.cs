6abf28a6b013765b4697151be36a1b4fa46ce0c8
Slagorforth <th.wettingfeld@gmail.com>
2022-12-29 21:16:28 +0100

    Task01Edit

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public float interactionRange = 2.0f;
+
+    private void Update()
+    {
+        // Prüfen, ob die Taste "E" gedrückt wurde
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Raycast von der Spielerposition in die Blickrichtung erstellen
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            // Raycast ausführen und prüfen, ob ein PC-Objekt getroffen wurde
+            if (Physics.Raycast(ray, out hit, interactionRange) && hit.collider.CompareTag("PC"))
+            {
+                // Interaktion mit dem PC-Objekt ausführen (z.B. Dialog anzeigen oder Daten abrufen)
+                Debug.Log("Interagiere mit PC-Objekt");
+            }
+        }
+    }
+}
