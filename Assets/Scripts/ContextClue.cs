5e7a08b4ca8c6e279148ce6cd468aca1b25bd41f
Slagorforth <th.wettingfeld@gmail.com>
2023-01-03 20:23:05 +0100

    Clue Context not working

---
+++ Assets/Scripts/ContextClue.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextClue : Interactable
+{
+    [SerializeField] private SpriteRenderer mySprite;
+    [SerializeField] private bool clueActive = false;
+    [SerializeField] private GameObject fragezeichen;
+
+
+    public void ChangeClue()
+    {
+        if (playerInRange)
+        {
+            clueActive = !clueActive;
+            //mySprite.enabled = clueActive;
+            fragezeichen.SetActive(true);
+        }
+
+        if(clueActive == true && !playerInRange)
+        {
+            clueActive = !clueActive;
+            fragezeichen.SetActive(false);
+        }
+        
+    }
+
+
+
+}
