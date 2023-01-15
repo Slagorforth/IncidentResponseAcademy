334529f15ce33851b0db3161bdca1f04fb0d26c7
Slagorforth <th.wettingfeld@gmail.com>
2023-01-15 12:53:47 +0100

    SwitchesAdded

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DoorType
+{
+    key,
+    enemy,
+    button
+}
+
+public class Door : Interactable
+{
+    [Header("Door variables")]
+    public DoorType thisDoorType;
+    public bool open = false;
+    public SpriteRenderer doorSprite;
+    public BoxCollider2D physicsCollider;
+
+    private void Update()
+    {
+    
+    }
+
+    public void Open()
+    {
+        //Turn off the door's sprite renderer
+        doorSprite.enabled = false;
+        //set open to true
+        open = true;
+        //turn off the door's box collider
+        physicsCollider.enabled = false;
+    }
+
+    public void Close()
+    {
+        //Turn off the door's sprite renderer
+        doorSprite.enabled = true;
+        //set open to true
+        open = false;
+        //turn off the door's box collider
+        physicsCollider.enabled = true;
+    }
+}
---
+++ Assets/Scripts/FalseSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalseSwitch : MonoBehaviour
+{
+    public bool active;
+    public BoolValue storedValue;
+    public Sprite activeSprite;
+    private SpriteRenderer mySprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+        active = storedValue.RuntimeValue;
+        if (active)
+        {
+            ActivateSwitch();
+        }
+    }
+
+    public void ActivateSwitch()
+    {
+        active = true;
+        storedValue.RuntimeValue = active;
+        mySprite.sprite = activeSprite;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // Is it the player?
+        if (other.CompareTag("Player"))
+        {
+            ActivateSwitch();
+        }
+    }
+}
