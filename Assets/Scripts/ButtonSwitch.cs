e6453469bdcebaa39941425f3c62beb6cc33a8c9
Slagorforth <th.wettingfeld@gmail.com>
2023-01-15 18:42:38 +0100

    ButtonSwitch Added

---
+++ Assets/Scripts/ButtonSwitch.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSwitch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+/*Bei Drücken der Leertaste -> Schalter springt um
+Bei richtigem Schalter -> Licht geht an sonst -> alle Lichter aus
+Wenn alle Lichter an -> Open Door
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public bool active;
+    public BoolValue storedValue;
+    public Sprite activeSprite;
+    private SpriteRenderer mySprite;
+    public Door thisDoor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+        active = storedValue.RuntimeValue;
+        if (active)
+        {
+            ActivateSwitch();
+        }
+    }
+
+    public void ActivateSwitch()
+    {
+        active = true;
+        storedValue.RuntimeValue = active;
+        thisDoor.Open();
+        mySprite.sprite = activeSprite;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // Is it the player?
+        if (other.CompareTag("Player"))
+        {
+            ActivateSwitch();
+        }
+    }
+}*/
